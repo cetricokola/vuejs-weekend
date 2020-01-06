5cc1a5a6eb579ba1304560b8503eed88537f8c37
cetRide <okolacetric@gmail.com>
2020-01-06 09:20:35 +0300

    axios

---
+++ vue-app/public/js/app.js
@@ -0,0 +1,17 @@
+Vue.prototype.$http = axios;
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            skills: []
+        }
+    },
+
+    mounted() {
+
+        this.$http.get('/skills').then(response => this.skills = response.data);
+
+    }
+
+});
